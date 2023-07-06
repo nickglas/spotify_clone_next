71c1b529145de2e953f0eac65417f035aba91ddd
Nick Glas <nickglas@hotmail.nl>
2023-07-06 13:58:52 +0200

    Spotify clone next.js 13

---
+++ app/search/error.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Box from "@/components/Box"
+
+const Error = () => {
+  return (
+    <Box className="g-full glex items-center justify-center">
+      <div className="text-neutral-400">Something went wrong</div>
+    </Box>
+  )
+}
+
+export default Error
---
+++ components/Library.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import {TbPlaylist} from "react-icons/tb"
+import {AiOutlinePlus} from "react-icons/ai"
+import useAuthModal from "@/hooks/useAuthModal"
+import { useUser } from "@/hooks/useUser"
+import useUploadModal from "@/hooks/useUploadModal"
+import { Song } from "@/types"
+import React from "react"
+import MediaItem from "./MediaItem"
+import useOnPlay from "@/hooks/useOnPlay"
+
+interface LibraryProps{
+  songs: Song[]
+}
+
+const Library: React.FC<LibraryProps> = ({songs}) => {
+  const authModal = useAuthModal()
+  const uploadModal = useUploadModal()
+  const {user} = useUser()
+
+  const onPlay = useOnPlay(songs)
+
+  const onClick = () => {
+    if(!user){
+      return authModal.onOpen()
+    }
+
+    return uploadModal.onOpen()
+
+  }  
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist className="text-neutral-400" size={26} />
+          <p className="text-neutral-400 font-medium text-md">Your library</p>
+        </div>
+        <AiOutlinePlus className="text-neutral-400 cursor-pointer hover:text-white transition" onClick={onClick} size={20}/>
+      </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((song) => (
+          <MediaItem 
+            key={song.id}
+            onClick={(id: string) => onPlay(id)} 
+            data={song}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Library
